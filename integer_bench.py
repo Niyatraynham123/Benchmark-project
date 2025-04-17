cb8d49b33c9ee692beb9c21dd5865568dde6660e
Niyatraynham123 <157864057+Niyatraynham123@users.noreply.github.com>
2025-04-16 22:02:20 -0400

    Created an integer and added one function called execute to integer_bench.py

---
+++ integer_bench.py
@@ -0,0 +1,12 @@
+import time
+ 
+ # Terminal color codes
+ COLOR_CODE_RED = "\x1b[31m"
+ COLOR_CODE_GREEN = "\x1b[32m"
+ COLOR_CODE_YELLOW = "\x1b[33m"
+ COLOR_CODE_RESET = "\x1b[0m"
+ 
+ def execute_benchmark():
+     accumulated_time = 0
+     test_value = 7
+     calculation_result = 0
